635bfc974b9cbef317f3aba9468e8387639f3d87
tn51108 <khwanchanok.sr@tnis.com>
2018-02-26 10:39:26 +0700

    update vending

--- vendingmc/main.go
+++ vendingmc/main.go
@@ -15,7 +15,19 @@ func (m vendingMachine) InsertedMoney() int {
 }
 
 func (m *vendingMachine) InsertCoin(coin string) {
-	m.insertedMoney = 10	
+	//m.insertedMoney = 10	
+	if coin == "T" {
+		m.insertedMoney += 10
+	}
+	if coin == "F" {
+		m.insertedMoney += 5
+	}
+	if coin == "TW" {
+		m.insertedMoney += 2
+	}
+	if coin == "O" {
+		m.insertedMoney += 1
+	}
 }
 func main() {
 	vm := vendingMachine{}
@@ -24,4 +36,7 @@ func main() {
 	vm.InsertCoin("T")
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	// Inserted Money: 10
+	vm.InsertCoin("F")
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
+	// Inserted Money: 5
 }
